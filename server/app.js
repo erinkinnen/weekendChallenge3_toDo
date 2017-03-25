6f000980f7b6e56ce89dacef76a6b481823e4a27
Erin Kinnen <erin.kinnen@outlook.com>
2017-03-25 13:05:40 -0500

    connection to db established, data received on client side

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 var port = 5000;
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 // var pg = require(pg);
+var tasks = require('./routes/tasks.js');
 
 app.use(express.static("server/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-
+app.use("/tasklist", tasks);
 
 
 
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
   console.log("jQuery sourced");
-});
+
+getTasksFromDB();
+
+});//end document ready
+
+function getTasksFromDB(){
+  $.ajax ({
+    type: "GET",
+    url: "/tasklist",
+    success: function(response){
+      console.log("tasklist GET success: ", response);
+    }
+  });//end of ajax
+}//end of getTasksFromDB
